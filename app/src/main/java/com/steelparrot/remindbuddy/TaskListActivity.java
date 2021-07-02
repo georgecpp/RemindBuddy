cc3ffdcf67b6051c9baafb95c2b5f53893f6fe9b
androcpp <georgeandronache.cpp@gmail.com>
2021-07-02 15:58:39 +0300

    SingleFragmentActivity pattern induction... WIP.

---
+++ app/src/main/java/com/steelparrot/remindbuddy/TaskListActivity.java
@@ -0,0 +1,13 @@
+package com.steelparrot.remindbuddy;
+
+import androidx.fragment.app.Fragment;
+
+// to do: implement TaskListFragment in order to get runnable app.
+public class TaskListActivity extends SingleFragmentActivity {
+
+    @Override
+    protected Fragment createFragment() {
+        return null;
+    }
+
+}
